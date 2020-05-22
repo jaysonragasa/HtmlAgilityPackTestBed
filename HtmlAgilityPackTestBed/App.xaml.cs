94c05306955c0f319be5ad76247b0f5a671b77b6
Jayson Ragasa <jayson.d.ragasa@live.com>
2020-05-22 15:50:46 +0800

    added appcenter stuff

--- HtmlAgilityPackTestBed/App.xaml.cs
+++ HtmlAgilityPackTestBed/App.xaml.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using System;
+using System.Deployment.Application;
+using System.Reflection;
 using System.Windows;
 
 namespace HtmlAgilityPackTestBed
@@ -13,5 +13,30 @@ namespace HtmlAgilityPackTestBed
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            AppCenter.Start("ae59173a-2296-4306-b03d-4db3a680d5af",
+                   typeof(Analytics), typeof(Crashes));
+
+#if DEBUG
+            Microsoft.AppCenter.Analytics.Analytics.TrackEvent("Running in DEBUG version " + getRunningVersion().ToString());
+#else
+            Microsoft.AppCenter.Analytics.Analytics.TrackEvent("Running in RELEASE version " + getRunningVersion().ToString());
+#endif
+        }
+
+        private Version getRunningVersion()
+        {
+            try
+            {
+                return ApplicationDeployment.CurrentDeployment.CurrentVersion;
+            }
+            catch (Exception)
+            {
+                return Assembly.GetExecutingAssembly().GetName().Version;
+            }
+        }
     }
 }
